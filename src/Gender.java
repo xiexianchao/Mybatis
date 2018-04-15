ae9758aafcca6164c94fd0f53ae3f879990183e2
xiechao <907796939@qq.com>
2018-04-15 19:03:30 +0800

    Initial commit

---
+++ src/Gender.java
@@ -0,0 +1,39 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author:
+ * @time:2018/1/18
+ * @description：
+ */
+public class Gender {
+    public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("D:\\java_project\\javaEE\\spring\\Mybatis\\src\\config\\generatorConfig.xml");
+        if(!configFile.exists())
+        {
+            System.out.println("文件不存在");
+        }
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+
+    }
+
+
+
+}
+
